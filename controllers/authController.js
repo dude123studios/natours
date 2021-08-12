acc227601d0eeb4d3c7fc5a04cb058df4390e78a
Atharv <dude123studios@gmail.com>
2021-08-12 16:04:02 -0700

    Added heroku config

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,16 +28,15 @@ const sendToken = (
     if (sendToken) {
         token = signToken(user._id);
         json.token = token;
-        const cookieOptions = {
+        res.cookie('jwt', token, {
             expires: new Date(
                 Date.now() +
                     process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
             ),
             secure: true,
             httpOnly: true,
-        };
-        if (process.env.NODE_ENV == 'production') cookieOptions.secure = true;
-        res.cookie('jwt', token);
+            secure: req.secure || req.headers['x-forwared-proto'] === 'https',
+        });
     }
     if (sendUser) json.data.user = user;
 
--- server.js
+++ server.js
@@ -35,3 +35,10 @@ process.on('unhandledRejection', (err) => {
         process.exit(1);
     });
 });
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM recieved. Shutting down gracefully');
+    server.close(() => {
+        console.log('Process terminated');
+    });
+});
