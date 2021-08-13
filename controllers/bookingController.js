0aaa50a0cee82e1f27ff89964b025527c9a81e31
Atharv <dude123studios@gmail.com>
2021-08-13 11:49:07 -0700

    Stripe Webhooks

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -22,7 +22,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
                 name: `${tour.name} Tour`,
                 description: tour.summary,
                 images: [
-                    `https://www.natours-dude123studios.herokuapp.com/img/tours/${tour.imageCover}`,
+                    `https://natours-dude123studios.herokuapp.com/img/tours/${tour.imageCover}`,
                 ],
                 amount: tour.price * 100,
                 currency: 'usd',
@@ -46,8 +46,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
     const tour = session.client_reference_id;
-    const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.display_items[0].amount / 100;
+    const user = (await User.findOne({ email: session.customer_details.email }))
+        .id;
+    const price = session.amount_total / 100;
     await Booking.create({ tour, user, price });
 };
 
