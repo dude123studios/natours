9a9e993b6a1c70636b39612ca94cf97179f9db50
Atharv <dude123studios@gmail.com>
2021-08-13 11:18:38 -0700

    Stripe Webhooks

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -22,7 +22,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
                 name: `${tour.name} Tour`,
                 description: tour.summary,
                 images: [
-                    `https://www.natours.dev/img/tours/${tour.imageCover}`,
+                    `https://www.natours-dude123studios.herokuapp.com/img/tours/${tour.imageCover}`,
                 ],
                 amount: tour.price * 100,
                 currency: 'usd',
@@ -47,7 +47,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.line_items[0].amount / 100;
+    const price = session.display_items[0].amount / 100;
     await Booking.create({ tour, user, price });
 };
 
@@ -63,7 +63,7 @@ exports.webhookCheckout = catchAsync(async (req, res, next) => {
     } catch (err) {
         return res.status(400).send(`Webhook error: ${err.message}`);
     }
-    if (event.type === 'checkout.session.complete') {
+    if (event.type === 'checkout.session.completed') {
         createBookingCheckout(event.data.object);
     }
     res.status(200).json({ received: true });
